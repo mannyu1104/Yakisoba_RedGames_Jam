296c3e2c28828f2f188684f380819a2447322eed
mannyu1104 <huangyuman8989@gmail.com>
2025-07-12 23:46:20 +0800

    working on calibration

--- Assets/Script/Accelerometer_Control.cs
+++ Assets/Script/Accelerometer_Control.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class Accelerometer_Control : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 2f;
+    private Vector3 _calibrationOffset;
+
+    private void Start()
+    {
+        CalibrateNeutralPosition();
+    }
 
     private void FixedUpdate()
     {
@@ -15,5 +21,12 @@ public class Accelerometer_Control : MonoBehaviour
     {
 
         transform.Rotate(Vector3.forward, -_moveSpeed * Input.acceleration.x);
+    }
+
+    public void CalibrateNeutralPosition()
+    {
+        // Store current accelerometer reading as the "neutral" position
+        _calibrationOffset = Input.acceleration;
+        Debug.Log("Calibrated neutral position: " + _calibrationOffset);
     }
 }
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,26 +2,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody _rb;
-    private SlideableObject _slideableObject;
-
     private Vector3 _velocityTilt;
-    private Vector3 _lastPosition;
     [SerializeField] private float _moveSpeed = 8f;
     [SerializeField] private float _stopLerpSpeed = 3f;
 
     private Vector3 _smoothLerpVelocity;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Vector3 _calibrationOffset;
+
+
+    private void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        CalibrateNeutralPosition();
     }
 
     private void FixedUpdate()
     {
-        if (_slideableObject == null) return;
-
         ControlMovement();
     }
 
@@ -44,22 +40,14 @@ public class PlayerMovement : MonoBehaviour
         _smoothLerpVelocity.z = Mathf.Lerp(_smoothLerpVelocity.z, zVelocity, Time.deltaTime * _stopLerpSpeed);
 
         Vector3 trayDelta = new Vector3(0f, 0f, _smoothLerpVelocity.z * Time.fixedDeltaTime);
-        Vector3 newPosition = _rb.position + trayDelta;
 
-        if (_slideableObject != null)
-        {
-            Vector3 adjusted = _slideableObject.AdjustVelocityToSlope(_smoothLerpVelocity);
-            _slideableObject.OnPlayerMove(adjusted);
-        }
-
-        _lastPosition = newPosition;
-
-        _rb.MovePosition(newPosition);
-
+        transform.Translate(trayDelta);
     }
 
-    public void GetCurrentFood(SlideableObject sObject)
+    public void CalibrateNeutralPosition()
     {
-        _slideableObject = sObject;
+        // Store current accelerometer reading as the "neutral" position
+        _calibrationOffset = Input.acceleration;
+        Debug.Log("Calibrated neutral position: " + _calibrationOffset);
     }
 }
