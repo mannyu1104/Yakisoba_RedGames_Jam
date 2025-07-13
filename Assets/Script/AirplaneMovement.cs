a75276690894f7b6d8c79afac52e500d26a13ed2
BingChiau <118031847+bingchiau@users.noreply.github.com>
2025-07-13 09:30:31 +0800

    Add airplane shake

--- Assets/Script/AirplaneMovement.cs
+++ Assets/Script/AirplaneMovement.cs
@@ -1,64 +1,72 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AirplaneMovement : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] private float _duration = 2f;
+    [Header("Setting")]
+    [SerializeField] private CinemachineImpulseSource _airplaneShake;
+    [SerializeField] private float _shakeForce = 0.3f;
+    [SerializeField] private Transform _tray;
 
     private float _timer;
-    private bool _isMovingDown;
-    private bool _isMovingUp;
-    private float _speed = 5f;
+    private float _cooldown = 20f;
+    private bool _isShake;
+    private Vector3 _velocity;
 
     private void Update()
     {
+
         _timer += Time.deltaTime;
-        
-        
 
-        if (_timer > _duration)
+        if (_timer > _cooldown)
         {
-            _timer = 0f; 
-            _isMovingDown = false;
-            _isMovingUp = false; 
+            if (!CheckTrayGotThing()) return;
+            _timer = 0;
+            AirplaneShake();
+            _isShake = true;
+            _cooldown = Random.Range(30, 60);
+        }
 
+        if (_isShake)
+        {
+            StartCoroutine(TrayShake());
+        }
+    }
+
+    private bool CheckTrayGotThing()
+    {
+        if (_tray.childCount == 0)
+        {
+            return false;
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                _timer = 0f;
-                _isMovingDown = true;
-
-            }
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                _timer = 0f;
-                _isMovingUp = true;
-            }
+            return true;
         }
-
-        if (_isMovingDown)
-        {
-            PlaneMoveDown();
-        }
-        if (_isMovingUp)
-        {
-            PlaneMoveUp();
-        }
-
     }
 
-    private void PlaneMoveUp()
+    private void AirplaneShake()
     {
-        _speed = 5f; 
-        transform.Rotate(Vector3.right * Time.deltaTime * _speed);
+        float x = Random.Range(-_shakeForce, _shakeForce);
+        float y = Random.Range(-_shakeForce, _shakeForce);
+        _velocity = new Vector3(x, y, 0);
+
+        _airplaneShake.GenerateImpulseWithVelocity(_velocity);
     }
 
-    private void PlaneMoveDown()
+    private IEnumerator TrayShake()
     {
-        _speed = 5f;
-        transform.Rotate(Vector3.left * Time.deltaTime * _speed);
+        _tray.transform.Rotate(Vector3.forward, -50f * Time.deltaTime);
+        yield return new WaitForSeconds(0.3f);
+        _tray.transform.Rotate(Vector3.forward, 100f * Time.deltaTime);
+        yield return new WaitForSeconds(0.3f);
+        _tray.transform.Rotate(Vector3.forward, -100f * Time.deltaTime);
+        yield return new WaitForSeconds(0.3f);
+        _tray.transform.Rotate(Vector3.forward, 50f * Time.deltaTime);
+        yield return new WaitForSeconds(0.3f);
+        _isShake = false;
     }
 }
