7bf980ae0093f5111c587afe5b94c1c88a1af9dc
Variety <104972808+Vareity438@users.noreply.github.com>
2025-07-13 13:40:29 +0800

    add more button

--- Assets/Script/SimpleMenuController.cs
+++ Assets/Script/SimpleMenuController.cs
@@ -9,30 +9,36 @@ public class SimpleMenuController : MonoBehaviour
     public GameObject menuPanel;
     public GameObject howToPlayPanel;
     public GameObject leaderboardPanel;
+    public GameObject changeControlPanel; // ← 加上这一行
     public Button howToPlayButton;
     public Button closeHowToPlayButton;
     public Button tapToStartButton;
     public Button leaderboardButton;
     public Button closeLeaderboardButton;
+    public Button changeControlButton;
 
-    [Header("Score Display")]
-    public TextMeshProUGUI highestScoreText; 
+    public Button closeChangeControlButton;
+
+    public TextMeshProUGUI highestScoreText;
 
     [Header("Scene Settings")]
     public string gameSceneName = "GameScene";
-
-    private bool isShowingHowToPlay = false;
 
     void Start()
     {
         howToPlayPanel.SetActive(false);
         leaderboardPanel.SetActive(false);
+        changeControlPanel.SetActive(false);
 
         howToPlayButton.onClick.AddListener(ShowHowToPlay);
         closeHowToPlayButton.onClick.AddListener(HideHowToPlay);
         leaderboardButton.onClick.AddListener(ShowLeaderboard);
         closeLeaderboardButton.onClick.AddListener(HideLeaderboard);
+        changeControlButton.onClick.AddListener(ShowChangeControlOptions);
+        closeChangeControlButton.onClick.AddListener(HideChangeControlOptions);
+
         tapToStartButton.onClick.AddListener(StartGame);
+
 
         int highestScore = PlayerPrefs.GetInt("HighestScore", 0);
         highestScoreText.text = "Highest Score: " + highestScore;
@@ -45,14 +51,12 @@ public class SimpleMenuController : MonoBehaviour
 
     public void ShowHowToPlay()
     {
-        isShowingHowToPlay = true;
         howToPlayPanel.SetActive(true);
         tapToStartButton.gameObject.SetActive(false);
     }
 
     public void HideHowToPlay()
     {
-        isShowingHowToPlay = false;
         howToPlayPanel.SetActive(false);
         tapToStartButton.gameObject.SetActive(true);
     }
@@ -66,4 +70,16 @@ public class SimpleMenuController : MonoBehaviour
     {
         leaderboardPanel.SetActive(false);
     }
+
+    public void ShowChangeControlOptions()
+    {
+        changeControlPanel.SetActive(true);
+        tapToStartButton.gameObject.SetActive(false);
+    }
+    public void HideChangeControlOptions()
+    {
+        changeControlPanel.SetActive(false);
+        tapToStartButton.gameObject.SetActive(true);
+    }
+
 }
