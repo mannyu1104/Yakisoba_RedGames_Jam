7f6bfe631f61a714ceb0ecb352a6c0cf6b4ce480
BingChiau <118031847+bingchiau@users.noreply.github.com>
2025-07-13 11:58:57 +0800

    Biggie leg out animation

--- Assets/Script/RandomLegOut.cs
+++ Assets/Script/RandomLegOut.cs
@@ -5,12 +5,19 @@ public class RandomLegOut : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private Transform[] _seats;
     [SerializeField] private float _changeInterval = 3f;
+    [SerializeField] private LayerMask whatIsCharacter;
 
     private float _timer;
     private bool _isSeatSelected;
     private bool _isBamHere;
     private bool _spawn = true;
     private Transform _selectedSeat;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -40,7 +47,7 @@ public class RandomLegOut : MonoBehaviour
         int randomIndex = Random.Range(0, _seats.Length);
         _selectedSeat = _seats[randomIndex];
 
-        _isBamHere = Physics.CheckBox(_selectedSeat.position, _selectedSeat.localScale / 2, Quaternion.identity, LayerMask.GetMask("Bam"));
+        _isBamHere = Physics.CheckSphere(_selectedSeat.position, 2f, whatIsCharacter);
 
         if (_isBamHere)
         {
@@ -48,6 +55,15 @@ public class RandomLegOut : MonoBehaviour
         }
         else if (!_isBamHere)
         {
+            _animator.SetBool("LegOut", false);
+            if (_selectedSeat.CompareTag("LeftSeat"))
+            {
+                transform.parent.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (_selectedSeat.CompareTag("RightSeat"))
+            {
+                transform.parent.localScale = new Vector3(1f, 1f, 1f);
+            }
             _isSeatSelected = true;
 
         }
@@ -56,10 +72,13 @@ public class RandomLegOut : MonoBehaviour
 
     private void MoveToTheSeat()
     {
+
         // Set the position of the GameObject to the selected seat
-        transform.position = new Vector3(_selectedSeat.position.x, _selectedSeat.position.y + 0.8f, _selectedSeat.position.z);
+        transform.parent.position = new Vector3(_selectedSeat.position.x, _selectedSeat.position.y + 0.8f, _selectedSeat.position.z);
+        Debug.Log(transform.gameObject.name);
         _isSeatSelected = false;
         _changeInterval = Random.Range(1f, 4f); // Randomize the next change interval
+        _animator.SetBool("LegOut", true);
     }
 
     private void StopNPC(float test, float test2)
@@ -67,13 +86,27 @@ public class RandomLegOut : MonoBehaviour
         _spawn = false;
     }
 
+    private void StartNPC()
+    {
+        _spawn = true;
+    }
+
     private void OnEnable()
     {
         AppyGravity.OnFoodDestroy += StopNPC;
+        GetMoney.OnSpawnFood += StartNPC;
     }
 
     private void OnDisable()
     {
         AppyGravity.OnFoodDestroy -= StopNPC;
+        GetMoney.OnSpawnFood -= StartNPC;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_selectedSeat == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_selectedSeat.position, 2f);
     }
 }
